35ffbd5b805baaa0cf40d1a9ac3d41456b4e1adc
AhmedEssamMo <aemo96@hotmail.com>
2021-07-17 07:28:41 +0200

    Commiting TrafficLight project

---
+++ S_AT_MC_05/App/TrafficLight/TrafficLight.h
@@ -0,0 +1,15 @@
+#ifndef TRAFFIC_LIGHT_H_
+#define TRAFFIC_LIGHT_H_
+#include"../../Mcal/UART/uart.h"
+#include"../../Firmware/LED/led.h"
+
+
+
+
+uint8_t TrafficLightUpdate(void);
+uint8_t TrafficLightInit(void);
+
+
+#endif  /*TRAFFIC_LIGHT_H_*/
+
+
