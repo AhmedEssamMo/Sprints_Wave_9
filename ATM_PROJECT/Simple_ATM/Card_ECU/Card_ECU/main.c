06749e005765bdec8b15041a9950bdd32f1e5277
AhmedEssamMo <aemo96@hotmail.com>
2021-08-02 17:21:07 +0200

    Commiting the ATM_Project without demos

---
+++ ATM_PROJECT/Simple_ATM/Card_ECU/Card_ECU/main.c
@@ -0,0 +1,19 @@
+/*
+ * Card_ECU.c
+ *
+ * Created: 7/24/2021 5:14:42 PM
+ * Author : Mohamed Wagdy
+ */ 
+
+#include "App/Card/Card.h"
+
+int main(void)
+{
+   CARD_Init();
+
+   /* Replace with your application code */
+   while (1) 
+   {
+   }
+}
+
