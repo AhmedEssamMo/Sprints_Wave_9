468faa7502e7da9b5e1b409f34b7734268c6e6a1
AhmedEssamMo <aemo96@hotmail.com>
2021-08-03 15:14:53 +0200

    Commiting for firrst time without unit test

---
+++ UART_drriver/App/EchoSystem/EchoSystem.h
@@ -0,0 +1,15 @@
+#ifndef TRAFFIC_LIGHT_H_
+#define TRAFFIC_LIGHT_H_
+#include"../../Mcal/UART/uart.h"
+#include"../../Firmware/LED/led.h"
+
+
+
+
+uint8_t AppInit(void);
+uint8_t AppUpdate(void);
+
+
+#endif  /*TRAFFIC_LIGHT_H_*/
+
+
---
+++ UART_drriver/Debug/App/EchoSystem/EchoSystem.d
@@ -0,0 +1,40 @@
+App/EchoSystem/EchoSystem.o App/EchoSystem/EchoSystem.o:  \
+ ../App/EchoSystem/EchoSystem.c ../App/EchoSystem/EchoSystem.h \
+  ../App/EchoSystem/../../Mcal/UART/uart.h \
+  ../App/EchoSystem/../../Mcal/UART/../../Infrastructure/TypeDef.h \
+  ../App/EchoSystem/../../Mcal/UART/../../Infrastructure/Macros.h \
+  ../App/EchoSystem/../../Mcal/UART/uartConfig.h \
+  ../App/EchoSystem/../../Firmware/LED/led.h \
+  ../App/EchoSystem/../../Firmware/LED/../../Infrastructure/Macros.h \
+  ../App/EchoSystem/../../Firmware/LED/../../Infrastructure/TypeDef.h \
+  ../App/EchoSystem/../../Firmware/LED/../../Mcal/DIO/DIO.h \
+  ../App/EchoSystem/../../Firmware/LED/../../Mcal/DIO/../../Infrastructure/Macros.h \
+  ../App/EchoSystem/../../Firmware/LED/../../Mcal/DIO/../../Infrastructure/TypeDef.h \
+  ../App/EchoSystem/../../Firmware/LED/../../Mcal/DIO/DIO_retval.h \
+  ../App/EchoSystem/../../Firmware/LED/led_config.h
+
+../App/EchoSystem/EchoSystem.h:
+
+../App/EchoSystem/../../Mcal/UART/uart.h:
+
+../App/EchoSystem/../../Mcal/UART/../../Infrastructure/TypeDef.h:
+
+../App/EchoSystem/../../Mcal/UART/../../Infrastructure/Macros.h:
+
+../App/EchoSystem/../../Mcal/UART/uartConfig.h:
+
+../App/EchoSystem/../../Firmware/LED/led.h:
+
+../App/EchoSystem/../../Firmware/LED/../../Infrastructure/Macros.h:
+
+../App/EchoSystem/../../Firmware/LED/../../Infrastructure/TypeDef.h:
+
+../App/EchoSystem/../../Firmware/LED/../../Mcal/DIO/DIO.h:
+
+../App/EchoSystem/../../Firmware/LED/../../Mcal/DIO/../../Infrastructure/Macros.h:
+
+../App/EchoSystem/../../Firmware/LED/../../Mcal/DIO/../../Infrastructure/TypeDef.h:
+
+../App/EchoSystem/../../Firmware/LED/../../Mcal/DIO/DIO_retval.h:
+
+../App/EchoSystem/../../Firmware/LED/led_config.h:
